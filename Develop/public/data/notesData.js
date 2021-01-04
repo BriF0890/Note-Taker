ae83dff8ddfceff9e2bbb460d5695da33fe67041
Brianca Foster <missbee51@yahoo.com>
2021-01-04 06:58:48 -0500

    Note-Taker

---
+++ Develop/public/data/notesData.js
@@ -0,0 +1,9 @@
+var notesArray = [
+    {
+      notes: "--------------",
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = notesArray;
+  
